c82f5e8f689a5932c2091b31efad41976bdf0374
Ximena Ibarra <xibarrab@gmail.com>
2021-11-25 08:25:37 -0500

    Primera carga

---
+++ src/test/java/issfademo/demo/CursoJavaIssfaApplicationTests.java
@@ -0,0 +1,13 @@
+package issfademo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJavaIssfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
